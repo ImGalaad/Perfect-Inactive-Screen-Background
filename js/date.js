e4092175f4a9d79c8ce4f0f3064e0aabcc7e7a96
Galaad <75391703+ImGalaad@users.noreply.github.com>
2022-02-12 23:57:05 +0100

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,25 +1,16 @@
 function clock() {
     var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-    var time = new Date(),
-        years = time.getFullYear(),
-        month = time.getMonth(),
-        date = time.getDate(),
-        day = time.getDay(),
-        hours = time.getHours(),
-        minutes = time.getMinutes(),
-        seconds = time.getSeconds();
+    var time = new Date(), years = time.getFullYear(), month = time.getMonth(),date = time.getDate(), day = time.getDay(),
+        hours = time.getHours(), minutes = time.getMinutes(),seconds = time.getSeconds();
 
     
     document.querySelectorAll('.clock')[0].innerHTML = zeroPadding(hours,2) + ":" + zeroPadding(minutes,2) + ":" + zeroPadding(seconds,2)
-    document.querySelectorAll('.date')[0].innerHTML = zeroPadding(years,4) + "/" + zeroPadding(month+1,2) + "/" + zeroPadding(date,2) + " | " + week[day];
-    }
-    setInterval(clock, 1000);
+    document.querySelectorAll('.date')[0].innerHTML = zeroPadding(years,4) + "/" + zeroPadding(month+1,2) + "/" + zeroPadding(date,2) + " | " + week[day];}
+    setInterval(clock, 1);
 
     
 function zeroPadding(num, digit) {
-    var zero = '';
+    var zero = "";
       for(var i = 0; i < digit; i++) {
-          zero += '0';
-      }
-      return (zero + num).slice(-digit);
-  };
+          zero += '0';}
+      return (zero + num).slice(-digit);};
